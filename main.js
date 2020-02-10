6b73a8a4e9b1bc501dc5f6e2609beee9c107f52d
Meternios <web.florianhitz@gmail.com>
2020-02-10 15:45:41 +0100

    Add files via upload
    Updated Drag and Drop, changed from HTML5 Drag and drop to mousedown

--- main.js
+++ main.js
@@ -1,13 +1,60 @@
-function allowDrop(ev) {
-    ev.preventDefault();
+const draggableElement = document.querySelector('.person-container');
+
+let currentDroppable = null;
+
+draggableElement.onmousedown = function(event) {
+
+    let shiftX = event.clientX - draggableElement.getBoundingClientRect().left;
+    let shiftY = event.clientY - draggableElement.getBoundingClientRect().top;
+
+    draggableElement.classList.add('animate-person');
+
+    moveAt(event.pageX, event.pageY);
+
+    function moveAt(pageX, pageY) {
+        draggableElement.style.left = pageX - shiftX + 'px';
+        draggableElement.style.top = pageY - shiftY + 'px';
+    }
+
+    function onMouseMove(event) {
+        moveAt(event.pageX, event.pageY);
+
+        draggableElement.hidden = true;
+        let elemBelow = document.elementFromPoint(event.clientX, event.clientY);
+        draggableElement.hidden = false;
+
+        if (!elemBelow) return;
+
+        let droppableBelow = elemBelow.closest('.canada-container');
+        if (currentDroppable != droppableBelow) {
+            if (currentDroppable) { // null when we were not over a droppable before this event
+                leaveDroppable(currentDroppable);
+            }
+            currentDroppable = droppableBelow;
+            if (currentDroppable) { // null if we're not coming over a droppable now
+                // (maybe just left the droppable)
+                enterDroppable(currentDroppable);
+            }
+        }
+    }
+
+    document.addEventListener('mousemove', onMouseMove);
+
+    draggableElement.onmouseup = function() {
+        document.removeEventListener('mousemove', onMouseMove);
+        draggableElement.onmouseup = null;
+    };
+
+};
+
+function enterDroppable(elem) {
+    elem.style.background = 'pink';
 }
 
-function drag(ev) {
-    ev.dataTransfer.setData("text", ev.target.id);
+function leaveDroppable(elem) {
+    elem.style.background = 'transparent';
 }
 
-function drop(ev) {
-    ev.preventDefault();
-    var data = ev.dataTransfer.getData("text");
-    ev.target.appendChild(document.querySelector(".person-container"));
-}
+draggableElement.ondragstart = function() {
+    return false;
+};
